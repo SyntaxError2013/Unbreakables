d48c2f5634d73d3e01d64285b4ab183a9f9b296a
Prakhar Singh <prakharsingh95@gmail.com>
2013-10-19 23:17:36 +0530

    Added a save function to save string of notes in wav format

--- play.py
+++ play.py
@@ -2,39 +2,48 @@ import subprocess
 import decimal
 
 #Usage
-# play(string  - The string containing notes
-# 	delay_step [OPTIONAL] - Delay between notes playback
+# play(notes  - The notes containing notes_array
+# 	delay_step [OPTIONAL] - Delay between notes_array playback
 # 	fade [OPTIONAL] - Fade time at end of playback
 # 	)
-def gen_string(string, delay_step = 0.05, fade = [0, 4, 0.1]):
+
+# save(file_name - The file to save to. Do not include an extension
+# 	notes  - The notes containing notes_array
+# 	delay_step [OPTIONAL] - Delay between notes_array playback
+# 	fade [OPTIONAL] - Fade time at end of playback
+# 	)
+
+def _gen_string(notes, delay_step = 0.05, fade = [0, 4, 0.1]):
 	# subprocess.call("play -n synth pl G2 pl B2 pl D3 pl G3 pl D4 pl G4 delay 0 .05 .1 .15 .2 .25 remix - fade 0 4 .1 norm -1", shell = True)
-	notes = string.split(" ")
-	result = "synth "
-	for i in notes:
-		result += "pl " + i + " "
+	notes_array = notes.split(" ")
+	final = "synth "
+	for i in notes_array:
+		final += "pl " + i + " "
 	if not delay_step == 0:
 		two_places = decimal.Decimal ('10') ** -2
 		j = 0
-		result += "delay "
-		while j < len(notes):			
-			result += str(decimal.Decimal(delay_step * j).quantize(two_places)) + " "
+		final += "delay "
+		while j < len(notes_array):			
+			final += str(decimal.Decimal(delay_step * j).quantize(two_places)) + " "
 			j += 1	
 
 	j = 0
-	result += "remix - fade "
+	final += "remix - fade "
 	while j < len(fade):
-		result += str(fade[j]) + " "
+		final += str(fade[j]) + " "
 		j += 1
 
-	result += "norm -1"
+	final += "norm -1"
 
-	return result
-	# subprocess.call(result, shell = True)
+	return final
+	# subprocess.call(final, shell = True)
 
-def play(string, delay_step = 0.05, fade = [0, 4, 0.1]):
-	subprocess.call("play -n " + gen_string("G2 B2 D3", delay_step, fade), shell = True)
+def play(notes, delay_step = 0.05, fade = [0, 4, 0.1]):
+	subprocess.call("play -n " + _gen_string(notes, delay_step, fade), shell = True)
 
-play("G2 B2 D3")
+def save(file_name, notes, delay_step = 0.05, fade = [0, 4, 0.1]):
+	subprocess.call("sox -n " + file_name + ".wav " + _gen_string(notes, delay_step, fade), shell = True)
+
 
 
 
