93f0ce71ed36ba40faf98cfa56258588cc1b8d71
Prakhar Singh <prakharsingh95@gmail.com>
2013-10-20 02:29:20 +0530

    Cleaned play.py and modified usage documentation

--- play.py
+++ play.py
@@ -2,36 +2,35 @@ import subprocess
 import decimal
 
 #Usage
-# play(notes  - The notes containing notes (string)
-# 	delay_step [OPTIONAL] - Delay between notes playback
+# play(	notes  - The string containing notes (eg: "A0 B3 E1 C4")
+# 	delay_period [OPTIONAL] - Delay between notes' playback
 # 	fade [OPTIONAL] - Fade time at end of playback
 # 	)
 
-# save(file_name - The file to save to. Do not include an extension
-# 	notes  - The notes containing notes_array
-# 	delay_step [OPTIONAL] - Delay between notes_array playback
+# save(	file_name - The file to save to. Do not include an extension
+# 	notes  -The string containing notes  (eg: "A0 B3 E1 C4")
+# 	delay_period [OPTIONAL] - Delay between notes' playback
 # 	fade [OPTIONAL] - Fade time at end of playback
 # 	)
 
-# save(file_name - The file to save to. Do not include an extension
+# save(	file_name - The file to save to. Do not include an extension
 # 	notes  - Array of a 2 element arrays (eg: [["A3 G2", 0.00], ["C3 D1", 0.07]])
-# 		The strum plays at the specified time.
-# 	delay_step [OPTIONAL] - Delay between notes_array playback
+# 		The strums play at the time specified in the second argument for each row.
+# 	delay_period [OPTIONAL] - Delay between notes' playback (within a particular strum)
 # 	fade [OPTIONAL] - Fade time at end of playback
 # 	)
 
-def _gen_string(notes, delay_step = 0.05, fade = [0, 4, 0.1]):
-	# subprocess.call("play -n synth pl G2 pl B2 pl D3 pl G3 pl D4 pl G4 delay 0 .05 .1 .15 .2 .25 remix - fade 0 4 .1 norm -1", shell = True)
+def _gen_string(notes, delay_period = 0.05, fade = [0, 4, 0.1]):
 	notes_array = notes.split(" ")
 	final = ""
 	for i in notes_array:
 		final += "pl " + i + " "
-	if not delay_step == 0:
+	if not delay_period == 0:
 		two_places = decimal.Decimal ('10') ** -2
 		j = 0
 		final += "delay "
 		while j < len(notes_array):			
-			final += str(decimal.Decimal(delay_step * j).quantize(two_places)) + " "
+			final += str(decimal.Decimal(delay_period * j).quantize(two_places)) + " "
 			j += 1	
 
 	j = 0
@@ -43,30 +42,28 @@ def _gen_string(notes, delay_step = 0.05, fade = [0, 4, 0.1]):
 		final += "norm -1"
 
 	return final
-	# subprocess.call(final, shell = True)
 
-def _gen_times(base, size, delay_step = 0.05):
+def _gen_times(base, size, delay_period = 0.05):
 	times = ""
 	j = 0
 	k = 0
 	two_places = decimal.Decimal ('10') ** -2
 	while j < size:
-		times += str(decimal.Decimal(base + delay_step * j).quantize(two_places)) + " "
+		times += str(decimal.Decimal(base + delay_period * j).quantize(two_places)) + " "
 		j += 1
 
-	k = delay_step * (j-1) + base
+	k = delay_period * (j-1) + base
 	return k, times
 
 
-def play(notes, delay_step = 0.05, fade = [0, 4, 0.1]):
-	subprocess.call("play -n synth " + _gen_string(notes, delay_step, fade), shell = True)
+def play(notes, delay_period = 0.05, fade = [0, 4, 0.1]):
+	subprocess.call("play -n synth " + _gen_string(notes, delay_period, fade), shell = True)
 
-def save(file_name, notes, delay_step = 0.05, fade = [0, 4, 0.1]):
-	subprocess.call("sox -n synth " + file_name + ".wav " + _gen_string(notes, delay_step, fade), shell = True)
+def save(file_name, notes, delay_period = 0.05, fade = [0, 4, 0.1]):
+	subprocess.call("sox -n synth " + file_name + ".wav " + _gen_string(notes, delay_period, fade), shell = True)
 
-def save(file_name, notes, delay_step = 0.05, fade = [0, 4, 0.1]):
+def save(file_name, notes, delay_period = 0.05, fade = [0, 4, 0.1]):
 	final = "sox -n " + file_name + ".wav synth "
-	# final = "play -n synth "
 	note_s = ""
 	for row in notes:
 		final += _gen_string(row[0], 0, [0, 0, 0])
@@ -78,7 +75,7 @@ def save(file_name, notes, delay_step = 0.05, fade = [0, 4, 0.1]):
 		base = 0
 		times = " "
 		for row in notes:
-			base, times = _gen_times(base + row[1], len(row[0].split(" ")), delay_step)
+			base, times = _gen_times(base + row[1], len(row[0].split(" ")), delay_period)
 			final += times
 
 	j = 0
