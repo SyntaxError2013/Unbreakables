5475f9499f079149eafbea15d17950504b234296
Prakhar Singh <prakharsingh95@gmail.com>
2013-10-20 05:16:03 +0530

    Cleaned share.py

--- share.py
+++ share.py
@@ -4,21 +4,19 @@ import requests
 # 	url [OPTIONAL] - URL to send the file to
 # 	)
 # returns:
-# 	ERROR_UPLOAD 100
-# 	ERROR_ALREADY_EXISTS 101
-# 	ERROR_NO_FILE_SENT 102
-# 	ERROR_MOVE_UPLOADED_FILE_FAILED 103
-# 	SUCCESS 150
-# 	ERROR_NO_RESPONSE 160
+#	ERROR_UPLOAD 100
+#	ERROR_ALREADY_EXISTS 101
+#	ERROR_NO_FILE_SENT 102
+#	ERROR_MOVE_UPLOADED_FILE_FAILED 103
+#	ERROR_NO_RESPONSE 160
+#	SUCCESS 150
 
 ERROR_NO_RESPONSE = 160
-def send(file_name, url = "http://localhost:8080"):
+def send(file_name, url):
 	files = { 'linkin_park' : open('music/' + file_name + '.wav', 'rb')}
 	r = requests.post(url, files=files)
 	if not r.text == '':
 		return int(r.text)
 	else:
 		return ERROR_NO_RESPONSE
-
-print send('sample')
 		
