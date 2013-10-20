58523007ff6d00d04523baf6ac85ccee8afa7c7f
Prakhar Singh <prakharsingh95@gmail.com>
2013-10-20 11:58:04 +0530

    Added python script for creating server

--- frets.py
+++ frets.py
@@ -3,82 +3,84 @@ import cv2
 import time
 import play
 
-vc = cv2.VideoCapture(0)
-ret, frame = vc.read()
+def init():
+	vc = cv2.VideoCapture(0)
+	ret, frame = vc.read()
 
-# Timer for saving strum timings and saving music
-start = time.time()
-gap = 0
-prevStrum = ""
-song = []
+	# Timer for saving strum timings and saving music
+	start = time.time()
+	gap = 0
+	prevStrum = ""
+	song = []
 
-# Motion detection flags
-prevPos = 0
-direction = 0
-up = 0
-down = 0
-firstframe = 1
+	# Motion detection flags
+	prevPos = 0
+	direction = 0
+	up = 0
+	down = 0
+	firstframe = 1
 
-while ret:
+	while ret:
 
-	# Change color space for better detection
-	hsvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	fingerImages = lib.fingerImages(hsvframe)
-	fingerPositions = lib.getPositions(fingerImages)
-	# Detect the mode of playback
-	mode = lib.getMode(fingerPositions)
+		# Change color space for better detection
+		hsvframe = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+		fingerImages = lib.fingerImages(hsvframe)
+		fingerPositions = lib.getPositions(fingerImages)
+		# Detect the mode of playback
+		mode = lib.getMode(fingerPositions)
 
-	# Show the mode on screen
-	cv2.putText(frame, mode , (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, 255)
-	cv2.imshow('preview', frame)	
-	
-	# Find the postion of lower strumming hand
-	lowerPos = lib.getLowerBlob(hsvframe)
-
-	# Motion detection for lower hand
-	if prevPos != 0:
-		disp = lowerPos[1] - prevPos
-		direction = direction + disp
-		if direction < -100:
-			up = 1
-			direction = 0
-		if direction > 100:
-			down = 1
-			direction = 0
-	if firstframe == 1:
-		firstframe = 0
-	prevPos = lowerPos[1]
+		# Show the mode on screen
+		cv2.putText(frame, mode , (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 4, 255)
+		cv2.imshow('preview', frame)	
 		
-	# Perform the playback and append the strum in song
-	if down == 1:
-		if gap == 1:
-			elapsed = time.time() - start
-			song.append([prevStrum, elapsed])
-			gap == 0
-		play.play(lib.get_strum(mode, 'down'))
-		if gap == 0:
-			start = time.time()
-			prevStrum = lib.get_strum(mode, 'down')
-			gap = 1
-		down = 0
+		# Find the postion of lower strumming hand
+		lowerPos = lib.getLowerBlob(hsvframe)
 
-	else:
-		if up == 1:
+		# Motion detection for lower hand
+		if prevPos != 0:
+			disp = lowerPos[1] - prevPos
+			direction = direction + disp
+			if direction < -100:
+				up = 1
+				direction = 0
+			if direction > 100:
+				down = 1
+				direction = 0
+		if firstframe == 1:
+			firstframe = 0
+		prevPos = lowerPos[1]
+			
+		# Perform the playback and append the strum in song
+		if down == 1:
 			if gap == 1:
 				elapsed = time.time() - start
 				song.append([prevStrum, elapsed])
 				gap == 0
-			play.play(lib.get_strum(mode, 'up'))
+			play.play(lib.get_strum(mode, 'down'))
 			if gap == 0:
 				start = time.time()
 				prevStrum = lib.get_strum(mode, 'down')
 				gap = 1
-			up = 0
+			down = 0
 
-	ret, frame = vc.read()
-	key = cv2.waitKey(20)
-	if key == 27:	#Ends the session and save the song. Press ESCAPE key
-		play.save('muse', song)
-		break
+		else:
+			if up == 1:
+				if gap == 1:
+					elapsed = time.time() - start
+					song.append([prevStrum, elapsed])
+					gap == 0
+				play.play(lib.get_strum(mode, 'up'))
+				if gap == 0:
+					start = time.time()
+					prevStrum = lib.get_strum(mode, 'down')
+					gap = 1
+				up = 0
 
-cv2.destroyAllWindows()
+		ret, frame = vc.read()
+		key = cv2.waitKey(20)
+		if key == 27:	#Ends the session and save the song. Press ESCAPE key
+			play.save('muse', song)
+			break
+
+	cv2.destroyAllWindows()
+
---
+++ server.py
@@ -0,0 +1,24 @@
+from bottle import route, run, template, static_file
+import play
+import frets
+
+root = '/home/prakhar/dev/se13/Unbreakables/public/'
+
+@route('/')
+def index():
+	return static_file('index.html', root=root, mimetype='text/html')
+
+@route('/madfrets')
+def index():
+	frets.init()
+	return "Initiated Capture"
+
+@route('/css/<filename:re:.*\.css>')
+def send_image(filename):
+    return static_file(filename, root=root + '/css/', mimetype='text/css')
+
+@route('/images/<filename:re:.*\.jpg>')
+def send_image(filename):
+    return static_file(filename, root=root + '/images/', mimetype='image/jpg')
+
+run(host='localhost', port=12000)
